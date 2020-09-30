b0d6465f878c8ec9dd08d3f7e37916c3db6e642f
Jiayu Li <jiayul3@student.unimelb.edu.au>
2020-09-30 14:27:11 +1000

    Added jars

--- src/db/DBConnection.java
+++ src/db/DBConnection.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-    private final String url = "jdbc:postgresql://localhost:5432/myDB";
-    private final String user = "postgres";
-    private final String password = "789456";
+//    private final String url = "jdbc:postgresql://localhost:5432/myDB";
+//    private final String user = "postgres";
+//    private final String password = "789456";
     /**
      * Connect to the PostgreSQL database
      * @return a Connection object
