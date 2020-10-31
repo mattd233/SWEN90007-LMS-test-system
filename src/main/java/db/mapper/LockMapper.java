fd22c5412546f563c0f7812420ebbe7a45c38441
Jiayu Li <jiayul3@student.unimelb.edu.au>
2020-10-31 22:38:59 +1100

    Removed unused code

--- src/main/java/db/mapper/LockMapper.java
+++ src/main/java/db/mapper/LockMapper.java
@@ -7,7 +7,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public class LockMapper {
+public class LockMapper extends Mapper {
 
     public static boolean hasKey(int lockable) {
         final String selectStmt = "SELECT * FROM exam_locks WHERE lockable = ?";
